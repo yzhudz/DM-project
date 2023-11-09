d176906fe92ad4693baf0d02f2a75a722a104642
Peter <463487141@qq.com>
2023-11-09 20:53:48 +0800

    update uss

--- evaluation.py
+++ evaluation.py
@@ -42,8 +42,9 @@ def f1_score_hyper_uss(estimation, ground_truth, a_value, truth_a_value):
 def aae_and_are(estimation, ground_truth):
     estimation.columns = [str(x) for x in estimation.columns.to_list()]
     ground_truth.columns = [str(x) for x in ground_truth.columns.to_list()]
-    list_key_estimated=estimation['index'].to_list()
-    fS_in_Phi = ground_truth[ground_truth['index'].apply(lambda x: x in list_key_estimated)].sort_values(by='index').reset_index(drop=True)
+    list_key_estimated = estimation['index'].to_list()
+    fS_in_Phi = ground_truth[ground_truth['index'].apply(lambda x: x in list_key_estimated)].sort_values(
+        by='index').reset_index(drop=True)
     Phi = estimation.sort_values(by='index').reset_index(drop=True)
     # aae = np.sum(np.sum(np.abs(Phi - fS_in_Phi), axis=1), axis=0) / Phi.shape[0]
     aae = np.mean(np.mean(np.abs(Phi.iloc[:, 1:] - fS_in_Phi.iloc[:, 1:])))
@@ -79,3 +80,29 @@ def f1_score_coco_uss(estimation, ground_truth):
     f1 = 2 * precision * recall / (precision + recall)
     return f1
 
+
+def f1_score_uss(estimation, ground_truth):
+    estimation.columns = [str(x) for x in estimation.columns.to_list()]
+    ground_truth.columns = [str(x) for x in ground_truth.columns.to_list()]
+    list_key_estimated = estimation.sort_values(by='0', ascending=False).iloc[:, 0].to_list()
+    list_key_ground_truth_top_n = ground_truth.sort_values(by='0', ascending=False).iloc[0:estimation.shape[0],
+                                  0].to_list()
+    list_key_ground_truth = ground_truth['index'].to_list()
+    out_estimation = []
+    for i in range(len(list_key_ground_truth)):
+        if list_key_ground_truth[i] not in list_key_estimated:
+            out_estimation.append(list_key_ground_truth[i])
+    tp = 0
+    for i in range(len(list_key_estimated)):
+        if list_key_estimated[i] in list_key_ground_truth_top_n:
+            tp += 1
+    fp = len(list_key_estimated) - tp
+
+    fn = 0
+    for i in range(len(out_estimation)):
+        if out_estimation[i] in list_key_ground_truth_top_n:
+            fn += 1
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * precision * recall / (precision + recall)
+    return f1
--- uss.py
+++ uss.py
@@ -6,31 +6,63 @@ import mmh3
 import utils as utils
 import numpy as np
 import ground_truth
+from evaluation import f1_score_uss, aae_and_are
+import pandas as pd
+
+
+class Node():
+    def __init__(self):
+        self.id = 0
+        self.value_list = []
 
 
 class OurUSS():
 
     def __init__(self):
         # self.file_path = file_path
-        self.words = []
+        # super().__init__(params)
+        # self.stream_item_set = []
+        self.bucket_size = 100
+        self.current_size = 0
+        self.bucket = {}
+        self.error = 0
         # self.exact_counter = defaultdict(int)
         # self.space_saving_count = defaultdict(int)
         # self.amount_of_words = 0
 
     def insert(self, key, value):
-        # get the arguments
-        self.params["key_count"]
-        pass
+        if key in self.bucket:
+            arr1 = np.array(self.bucket[key][2])
+            arr2 = np.array(value)
+            self.bucket[key][0] += 1
+            self.bucket[key][2] = list(arr1 + arr2)
+        elif len(self.bucket) + 1 <= self.bucket_size:
+            self.bucket[key] = [1, self.error, value]
+        else:
+            min_error = float(np.inf)
+            for i in self.bucket:
+                # print(i)
+                if self.bucket[i][0] + self.bucket[i][1] < min_error:
+                    min_error = self.bucket[i][0] + self.bucket[i][1]
+            self.error = min_error
+            keys = [v for i, v in enumerate(self.bucket) if self.bucket[v][0] + self.bucket[v][1] <= self.error]
+            for key in keys:
+                if random.uniform(0, 1) < 1 / (self.error + 1):
+                    self.bucket.pop(key)
+            self.bucket[key] = [1, self.error, value]
 
     def all_query(self):
-        pass
+        self.bucket = dict(
+            sorted(self.bucket.items(), key=lambda i: i[1][0] + i[1][1], reverse=True))
+        result = {i: self.bucket[i][2] for i in self.bucket}
+        return result
 
     def read_file(self):
         with open('synthetic_dataset/synthetic_dataset.txt') as f:
             for line in f:
                 row = line.split()
                 value = [int(x) for x in row]
-                self.words.append(value)
+                self.stream_item_set.append(value)
         # print(self.words)
 
     # def space_saving_counter(self, k):
@@ -50,24 +82,77 @@ class OurUSS():
     def unbiased_space_saving_counter(self, k):
         """ Space saving counter """
         counters = {}
-        for word in self.words:
-            if word[0] in counters:
-                counters[word[0]] += 1
-            elif len(counters) + 1 < k:
-                counters[word[0]] = 1
+        for word in self.stream_item_set:
+            key = word[0]
+            if key in counters:
+                counters[key] += 1
+            elif len(counters) + 1 <= k:
+                counters[key] = 1
             else:
-                min_counter = min(counters, key=counters.get)
-                if random.uniform(0, 1) < 1 / (min_counter + 1):
-                    counters[word[0]] = counters.pop(min_counter) + 1
-                else:
-                    counters[min_counter] += 1
+                min_counter_key = min(counters, key=counters.get)
+                if random.uniform(0, 1) < 1 / (min_counter_key + 1):
+                    counters[key] = counters.pop(min_counter_key) + 1
+                # else:
+                #     counters[min_counter_key] += 1
+            # print(values)
         self.space_saving_count = counters
         self.space_saving_count = dict(sorted(self.space_saving_count.items(), key=lambda item: item[1], reverse=True))
         print(self.space_saving_count)
+        print(len(self.space_saving_count))
+
+    def unbiased_space_saving_value(self, k):
+        """ Space saving counter """
+        bucket = {}
+        error = 0
+        for item in self.stream_item_set:
+            key = item[0]
+            value = item[1:]
+            if key in bucket:
+                arr1 = np.array(bucket[key][2])
+                arr2 = np.array(value)
+                bucket[key][0] += 1
+                bucket[key][2] = list(arr1 + arr2)
+            elif len(bucket) + 1 <= k:
+                bucket[key] = [1, error, value]
+            else:
+                min_error = float(np.inf)
+                for i in bucket:
+                    # print(i)
+                    if bucket[i][0] + bucket[i][1] < min_error:
+                        min_error = bucket[i][0] + bucket[i][1]
+                error = min_error
+                keys = [v for i, v in enumerate(bucket) if bucket[v][0] + bucket[v][1] <= error]
+                for key in keys:
+                    if random.uniform(0, 1) < 1 / (error + 1):
+                        bucket.pop(key)
+                bucket[key] = [1, error, value]
+
+        bucket = dict(
+            sorted(bucket.items(), key=lambda i: i[1][0] + i[1][1], reverse=True))
+        print(bucket)
+        # print(self.space_saving_count)
+        # print(values)
 
 
 if __name__ == "__main__":
     uss = OurUSS()
-    uss.read_file()
-    uss.unbiased_space_saving_counter(100)
+    # uss.read_file()
+    groundTruth = ground_truth.GroundTruth({"value_count": 5, "normalization": False})
+    with open('synthetic_dataset/synthetic_dataset.txt') as f:
+        for line in f:
+            row = line.split()
+            value = [int(x) for x in row]
+            # uss.stream_item_set.append(value)
+            uss.insert(value[0], value[1:])
+            groundTruth.insert(value[0], value[1:])
+
+    result = uss.all_query()
+    result2, _ = groundTruth.all_query()
+    print('f1 score: ',
+          f1_score_uss(pd.DataFrame(result).T.reset_index(), pd.DataFrame(result2).T.reset_index()))
+    aae, are = aae_and_are(pd.DataFrame(result).T.reset_index(), pd.DataFrame(result2).T.reset_index())
+    print('AAE: ', aae)
+    print('ARE: ', are)
+
     # uss.unbiased_space_saving_counter(100)
+    # uss.unbiased_space_saving_value(100)
